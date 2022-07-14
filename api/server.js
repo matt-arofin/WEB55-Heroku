1f45ffa124af661072f877c89abf468090ad9e80
Matthew Arofin <Matt.arofin@gmail.com>
2022-07-14 12:57:58 -0400

    'start script refactor'

--- api/server.js
+++ api/server.js
@@ -7,4 +7,8 @@ server.get('/', (req, res) => {
     res.send('<h1>The server is up!</h1>')
 })
 
+server.get('/message-of-the-day', (req, res) => {
+    res.send(`<h1>Today's message:</h1><p>${process.env.MOTD || '(no message)'}</p>`)
+})
+
 module.exports = server
